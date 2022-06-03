3c07bcf41ab3487be5eb457a3df8e77192853eab
Sam K <sakw975257@gmail.com>
2022-06-03 18:54:37 -0500

    Add unit toggle feature and other fixes.

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
+function getUnit() {
+    let isChecked = document.getElementById("switchValue").checked;
+    return isChecked
+}
+
+let lastCity = "Washington D.C."
+
 let weather = {
     "apiKey": "b66ff9824239561c2a5cd92cf2ecfe38",
     fetchWeather: function(city) {
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey
-        )
-            .then((response) => response.json())
-            .then((data) => this.displayWeather(data));
+        getUnit();
+        if (getUnit() === false) {
+            fetch(
+                "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey
+            )
+                .then(function(response) {
+                    if (!response.ok) {
+                        alert("Sorry, that location could not be found.");
+                    }
+                    else {
+                        return response.json()
+                    }
+                })
+                .then(function(data) {
+                    weather.displayWeather(data);
+                })
+        }
+        else {
+            fetch(
+                "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey
+            )
+                .then(function(response) {
+                    if (!response.ok) {
+                        alert("Sorry, that location could not be found.");
+                    }
+                    else {
+                        return response.json()
+                    }
+                })
+                .then(function(data) {
+                    weather.displayWeather(data);
+                })
+        }
+        lastCity = city
     },
     displayWeather: function(data) {
         const {name} = data;
@@ -15,27 +51,65 @@ let weather = {
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
         document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°F";
+        if (getUnit() === false) {
+            document.querySelector(".temp").innerText = temp + "°F";
+        }
+        else {
+            document.querySelector(".temp").innerText = temp + "°C";
+        }
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " mph";
+        if (getUnit() === false) {
+            document.querySelector(".wind").innerText = "Wind Speed: " + speed + " mph";
+        }
+        else {
+            document.querySelector(".wind").innerText = "Wind Speed: " + speed + " m/s";
+        }
         document.querySelector(".weather").classList.remove("loading")
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + ",building,nature')"
     },
     search: function() {
         this.fetchWeather(document.querySelector(".search-bar").value);
     }
 };
 
-document
-    .querySelector("button")
-    .addEventListener("click", function() {
-        weather.search();
-    });
-
-document.querySelector(".search-bar").addEventListener("keyup", function(event) {
-    if (event.key == "Enter") {
+document.querySelector("button").addEventListener("click", function() {
+    if (document.querySelector(".search-bar").value.length === 0) {
+        weather.fetchWeather(lastCity);
+    }
+    else {
         weather.search();
     }
 });
 
+document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        if (document.querySelector(".search-bar").value.length === 0) {
+            weather.fetchWeather(lastCity);
+        }
+        else {
+            weather.search();
+        }
+    }
+});
+
+document.querySelector("#switchValue").addEventListener("click", function() {
+    if (document.querySelector(".search-bar").value.length === 0) {
+        weather.fetchWeather(lastCity);
+    }
+    else {
+        weather.search();
+    }
+
+    let content = document.querySelector(".switch").title
+    if (content == "Units (Imperial):") {
+        document.querySelector(".switch").title = "Units (Metric):";
+    }
+    else {
+        document.querySelector(".switch").title = "Units (Imperial):";
+    }
+});
+
 weather.fetchWeather("Washington D.C.");
+
+//change images so it also gets
+//weather conditions
